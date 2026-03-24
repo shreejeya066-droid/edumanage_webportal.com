52d583e725d888557c557d830a61188da9dc86fc
Jeya <shreejeya066@gmail.com>
2026-03-24 21:29:20 +0530

    success

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = 'https://student-backend-osum.onrender.com/api';
 
 export const fetchStudents = async () => {
     try {
